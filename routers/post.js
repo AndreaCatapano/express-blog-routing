8c8b8c7f00c1de1a158c0a1e38198e84bba382c8
AndreaCatapano <a.catapano96@gmail.com>
2025-03-27 13:41:02 +0100

    feat: added update, modify and destroy call

--- routers/post.js
+++ routers/post.js
@@ -18,6 +18,21 @@ router.post("/", function(req, res) {
     res.send("Nuovo Post")
 })
 
+router.put("/:id", function(req, res) {
+    const id = req.params.id;
+    res.send(`Modificato completamente il post ${id}`);
+})
+
+router.patch("/:id", function(req, res) {
+    const id = req.params.id;
+    res.send(`Modifica parziale per il post ${id}`);
+})
+
+router.delete("/:id", function(req, res) {
+    const id = req.params.id;
+    res.send(`Eliminato l'elemento ${id}`);
+})
+
 
 module.exports = router;
 
